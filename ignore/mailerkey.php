437f0f2fe6cd7d2ddd383dc749d7bac3f671e5dc
Patrick Viker <pviker@me.com>
2016-08-29 16:03:05 -0500

    ignore

--- ignore/mailerkey.php
+++ ignore/mailerkey.php
@@ -1,5 +1,4 @@
 <?php
-
-	putenv('SENDGRID_API_KEY=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');
+putenv ( 'SENDGRID_API_KEY=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' );
 
 ?>
